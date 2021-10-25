bacfe492dfebb0cad0fb00f803629303c29db0c0
tamara96mc <tamara.96mc@gmail.com>
2021-10-25 16:22:29 +0200

    cambios en el modelo de provincia

--- models/provincia.js
+++ models/provincia.js
@@ -3,26 +3,27 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class provincias extends Model {
+  class provincia extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+    //  */
     static associate(models) {
       movie.belongsTo(models.ca, {
         foreignKey: 'caId'
       });
     }
   };
-  provincias.init({
+  provincia.init({
     cp: DataTypes.INTEGER,
     nombre: DataTypes.STRING,
     poblacion: DataTypes.STRING,
-    superficie: DataTypes.INTEGER
+    superficie: DataTypes.INTEGER,
+    caId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'provincias',
+    modelName: 'provincia',
   });
-  return provincias;
+  return provincia;
 };
